33dfcc0cda3f3574d9438af10581ee03a87ad4e5
Ken <Ken.rubin@live.com>
2015-03-31 01:46:19 -0400

    more look and feel

--- public/js/main.js
+++ public/js/main.js
@@ -7,6 +7,42 @@ $(document).ready(function () {
 	
 	try {
 
+		// canvas.
+		var canvas = document.getElementById("surfacecanvas");
+		var context = canvas.getContext("2d");
+		context.fillStyle = "rgba(0,0,0,1)";
+		context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+
+		context.fillStyle = "#333";
+		for (var iX = 0; iX < context.canvas.width; iX+=(1024/16)) {
+
+			context.fillRect(iX, 0, 1, context.canvas.height);
+		}
+		for (var iY = 0; iY < context.canvas.height; iY+=(768/16)) {
+
+			context.fillRect(0, iY, context.canvas.width, 1);
+		}
+		context.fillStyle = "#ccc";
+		for (var i = 0; i < 9; i++) {
+
+			var iDivisor = Math.pow(2, i);
+			var iXStep = context.canvas.width / iDivisor;
+			var iYStep = context.canvas.height / iDivisor;
+
+			for (var iX = 0; iX < context.canvas.width; iX+=iXStep) {
+
+				context.fillRect(iX, 0, 1, iYStep);
+				context.fillRect(context.canvas.width - iX, context.canvas.height, 1, -iYStep);
+			}
+			for (var iY = 0; iY < context.canvas.height; iY+=iYStep) {
+
+				context.fillRect(0, iY, iXStep, 1);
+				context.fillRect(context.canvas.width, context.canvas.height - iY, -iXStep, 1);
+			}
+		}
+		context.fillRect(context.canvas.width, context.canvas.height, -1, -context.canvas.height);
+		context.fillRect(context.canvas.width, context.canvas.height, -context.canvas.width, -1);
+
 		$(".paletteitem").click(function () {
 
 			$(".paletteitem").removeClass("projectsitembuttonhighlight");
