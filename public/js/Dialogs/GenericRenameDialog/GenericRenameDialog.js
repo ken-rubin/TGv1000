cf83ef49340dc8826567f6765b793cb2cb94f98e
Jerry Rubin <jerry@rubintech.com>
2015-07-06 12:37:48 -0400

    SS

--- public/js/Dialogs/GenericRenameDialog/GenericRenameDialog.js
+++ public/js/Dialogs/GenericRenameDialog/GenericRenameDialog.js
@@ -5,161 +5,213 @@
 //
 
 // Define the module.
-define(["Core/snippetHelper", "Core/errorHelper", "Core/resourceHelper", "Code/Types"], 
-  function (snippetHelper, errorHelper, resourceHelper, Types) {
+define(["Core/snippetHelper", "Core/errorHelper", "Core/resourceHelper", "Code/Types"],
+	function(snippetHelper, errorHelper, resourceHelper, Types) {
 
-    try {
+		try {
 
-      // Define the GenericRenameDialog constructor function.
-      var functionGenericRenameDialog = function () {
+			// Define the GenericRenameDialog constructor function.
+			var functionGenericRenameDialog = function() {
 
-        try {
+				try {
 
-          var self = this;      // Uber closure.
+					var self = this; // Uber closure.
 
-          //////////////////////////////////
-          // Public methods.
+					//////////////////////////////////
+					// Public methods.
 
-          // Create and show Bootstrap dialog.
-          self.create = function(objectType, index) {
+					// Create and show Bootstrap dialog.
+					self.create = function(objectType, index) {
 
-            try {
+						try {
 
-              m_strObjectType = objectType;
-              m_iIndex = index;
+							m_strObjectType = objectType;
+							m_iIndex = index;
 
-              // Get the dialog DOM.
-              $.ajax({
+							// Get the dialog DOM.
+							$.ajax({
 
-                cache: false,
-                data: { 
+								cache: false,
+								data: {
 
-                  templateFile: "Dialogs/GenericRenameDialog/GenericRenameDialog"
-                }, 
-                dataProperty: "HTML",
-                method: "POST",
-                url: "/renderJadeSnippet"
-              }).done(m_functionRenderJadeSnippetResponse).error(errorHelper.show);
+									templateFile: "Dialogs/GenericRenameDialog/GenericRenameDialog"
+								},
+								dataProperty: "HTML",
+								method: "POST",
+								url: "/renderJadeSnippet"
+							}).done(m_functionRenderJadeSnippetResponse).error(errorHelper.show);
 
-              return null;
-            } catch (e) {
+							return null;
+						} catch (e) {
 
-              return e;
-            }
-          };
+							return e;
+						}
+					};
 
-          self.closeYourself = function() {
+					self.closeYourself = function() {
 
-            m_dialog.close();
-          }
+						m_dialog.close();
+					}
 
-          //////////////////////////////////
-          // Private methods.
+					//////////////////////////////////
+					// Private methods.
 
-          // Have converted jade of dialog to HTML. Open its dialog.
-          var m_functionRenderJadeSnippetResponse = function (htmlData) {
+					// Have converted jade of dialog to HTML. Open its dialog.
+					var m_functionRenderJadeSnippetResponse = function(htmlData) {
 
-            try {
+						try {
 
-              // Show the dialog--load the content from 
-              // the PropertysDialog jade HTML-snippet.
-              BootstrapDialog.show({
+							// Show the dialog--load the content from 
+							// the PropertysDialog jade HTML-snippet.
+							BootstrapDialog.show({
 
-                title: "Rename",
-                size: BootstrapDialog.SIZE_WIDE,
-                      message: $(htmlData),
-                      buttons: [
-                        {
-                          label: "Rename",
-                          id: 'RenameBtn',
-                          cssClass: "btn-primary",
-                          action: function(){
+								title: "Rename",
+								size: BootstrapDialog.SIZE_WIDE,
+								message: $(htmlData),
+								buttons: [{
+									label: "Rename",
+									id: 'RenameBtn',
+									cssClass: "btn-primary",
+									action: function() {
 
-                            m_functionRename();
-                          }
-                        },
-                        {
-                            label: "Close",
-                            icon: "glyphicon glyphicon-remove-circle",
-                            cssClass: "btn-warning",
-                            action: function(dialogItself){
+										m_functionRename();
+									}
+								}, {
+									label: "Close",
+									icon: "glyphicon glyphicon-remove-circle",
+									cssClass: "btn-warning",
+									action: function(dialogItself) {
 
-                                dialogItself.close();
-                            }
-                        }
-                      ],
-                      draggable: true,
-                      onshown: m_functionOnShownDialog
-                  });
-            } catch (e) {
+										dialogItself.close();
+									}
+								}],
+								draggable: true,
+								onshown: m_functionOnShownDialog
+							});
+						} catch (e) {
 
-              errorHelper.show(e);
-            }
-          };
+							errorHelper.show(e);
+						}
+					};
 
-          // Wire up Property handlers to dialog controls.
-          var m_functionOnShownDialog = function (dialogItself) {
+					// Wire up Property handlers to dialog controls.
+					var m_functionOnShownDialog = function(dialogItself) {
 
-            try {
+						try {
 
-              // Save the dailog object reference.
-              m_dialog = dialogItself;
+							// Save the dailog object reference.
+							m_dialog = dialogItself;
 
-              var strType = m_strObjectType.charAt(0).toUpperCase() + m_strObjectType.substring(1);
-              var strName = '';
-              if (m_strObjectType === 'type') {
+							var strType = m_strObjectType.charAt(0).toUpperCase() + m_strObjectType.substring(1);
+							var strName = '';
+							if (m_strObjectType === 'type') {
 
-                strName = types.getActiveClType().data.name;
+								strName = types.getActiveClType().data.name;
 
-              } else if (m_strObjectType === 'method') {
+							} else if (m_strObjectType === 'method') {
 
-                strName = types.getActiveClType().data.methods[m_iIndex].name;
+								strName = types.getActiveClType().data.methods[m_iIndex].name;
 
-              } else if (m_strObjectType === 'event') {
+							} else if (m_strObjectType === 'event') {
 
-                strName = types.getActiveClType().data.events[m_iIndex].name;
+								strName = types.getActiveClType().data.events[m_iIndex].name;
 
-              } else {
+							} else {
 
-                throw new Error('Invalid objectType passed to Rename Dialog.');
-              }
+								throw new Error('Invalid objectType passed to Rename Dialog.');
+							}
 
-              $("#RenameLabel").text("Enter a new name for the " + strType + ":");
-              $("#RenameInput").val(strName);
+							$("#RenameLabel").text("Enter a new name for the " + strType + ":");
+							$("#RenameInput").val(strName);
 
-            } catch (e) {
+						} catch (e) {
 
-              errorHelper.show(e);
-            }
-          };
+							errorHelper.show(e);
+						}
+					};
 
-        } catch (e) {
+				} catch (e) {
 
-          errorHelper.show(e);
-        }
+					errorHelper.show(e);
+				}
 
-        var m_functionRename = function () {
+				var m_functionRename = function() {
 
-          
-        }
+					try {
 
-        /////////////////////////////////
-        // Private fields.
+						// Check for empty.
+						var strName = $("#RenameInput").val().trim();
+						if (strName.length === 0) {
 
-        // Reference to the dialog object instance.
-        var m_dialog = null;
-        // Object type we're potentially deleting ('type','method','property','event')
-        var m_strObjectType = "";
-        // For 'method', 'property', 'event': Index in  array respective array in Types#m_clTypeActive.
-        // For 'type': Index in array Types#m_arrayClTypes.
-        var m_iIndex = -1;
-      };
+							errorHelper.show("You must enter a name.");
+							return;
+						}
 
-      // Return the constructor function as the module object.
-      return functionGenericRenameDialog;
+						// Check for duplicate.
+						if (m_strObjectType === 'type') {
 
-    } catch (e) {
+							if (!client.isTypeNameAvailableInActiveComic(strName, m_iIndex)) {
 
-      errorHelper.show(e);
-    }
-  });
+								errorHelper.show('That name is already in use. Please try another.');
+								return;
+							}
+
+
+
+
+						} else if (m_strObjectType === 'method') {
+
+
+							if (!client.isMethodNameAvailableInActiveType(strName, m_iIndex)) {
+
+								errorHelper.show('That name is already in use. Please try another.');
+								return;
+							}
+							
+
+
+							
+						} else if (m_strObjectType === 'event') {
+
+
+							if (!client.isEventNameAvailableInActiveType(strName, m_iIndex)) {
+
+								errorHelper.show('That name is already in use. Please try another.');
+								return;
+							}
+
+
+
+							
+						} else {
+
+							throw new Error("Invalid objectType passed to Rename Dialog.")
+						}
+
+					} catch (e) {
+
+						errorHelper.show(e);
+					}
+
+				}
+
+				/////////////////////////////////
+				// Private fields.
+
+				// Reference to the dialog object instance.
+				var m_dialog = null;
+				// Object type we're potentially deleting ('type','method','property','event')
+				var m_strObjectType = "";
+				// For 'method', 'property', 'event': Index in  array respective array in Types#m_clTypeActive.
+				// For 'type': Index in array Types#m_arrayClTypes.
+				var m_iIndex = -1;
+			};
+
+			// Return the constructor function as the module object.
+			return functionGenericRenameDialog;
+
+		} catch (e) {
+
+			errorHelper.show(e);
+		}
+	});
