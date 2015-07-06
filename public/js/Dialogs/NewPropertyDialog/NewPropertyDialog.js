3c920f644b4add63afc2d2778b00aa540387e710
Jerry Rubin <jerry@rubintech.com>
2015-07-06 07:43:55 -0400

    Add/Edit Property is done. Needs more testing.

--- public/js/Dialogs/NewPropertyDialog/NewPropertyDialog.js
+++ public/js/Dialogs/NewPropertyDialog/NewPropertyDialog.js
@@ -144,7 +144,7 @@ define(["Core/snippetHelper", "Core/errorHelper", "Core/resourceHelper"],
 
 										$("#TrueRadio").prop("checked", true);
 
-									} else {
+									} else if (m_property.initialValue === 'false') {
 
 										$("#FalseRadio").prop("checked", true);
 
@@ -251,6 +251,12 @@ define(["Core/snippetHelper", "Core/errorHelper", "Core/resourceHelper"],
 
 						try {
 
+							if (m_iDataType === 0) {
+
+								errorHelper.show("Please select a Data type.");
+								return;
+							}
+
 							var propertyName = $("#PropertyName").val().trim();
 							
 							if (!client.isPropertyNameAvailableInActiveType(propertyName)) {
@@ -259,17 +265,80 @@ define(["Core/snippetHelper", "Core/errorHelper", "Core/resourceHelper"],
 								return;
 							}
 
+							var strInitialValue = "";
+							switch (m_iDataType) {
+								case 1:
+									strInitialValue = $("#NumberOrStringInitial").val().trim();
+									if (strInitialValue.length > 0) {
+
+										if (!$.isNumeric(strInitialValue)) {
+
+											errorHelper.show("Initial value must be numeric.");
+											return;
+										}
+									}
+									break;
+								case 2:
+									var strFrom = $("#RangeFromInitial").val().trim();
+									var strThru = $("#RangeThruInitial").val().trim();
+									var num = (strFrom.length > 0 ? 1 : 0) + (strThru.length > 0 ? 1 : 0);
+									if (num > 0) {
+
+										if (num === 1) {
+
+											errorHelper.show("A Number range requires both from and thru.");
+											return;
+										}
+
+										if (!$.isNumeric(strFrom) || !$.isNumeric(strThru)) {
+
+											errorHelper.show("Non-numeric values are not allowed for Number ranges.");
+											return;
+										}
+
+										var numFrom = parseFloat(strFrom);
+										var numThru = parseFloat(strThru);
+										if (numThru < numFrom) {
+
+											errorHelper.show("Please reverse your numbers. The first must be less than the second.");
+											return;
+										}
+
+										strInitialValue = strFrom + '-' + strThru;
+									}
+									break;
+								case 3:
+									strInitialValue = $("#NumberOrStringInitial").val().trim();
+									break;
+								case 4:
+									if ($('input[value=1').prop("checked")) {
+
+										strInitialValue = 'true';
+
+									} else if ($('input[value=0').prop("checked")) {
+
+										strInitialValue = 'false';
+									}
+									break;
+								case 5:
+									var strCheck = $("#PicklistInitial").val().trim();
+									if (strCheck.length > 0) {
+
+										var picks = strCheck.match(/([\w\-]+)/g);
+										strInitialValue = picks.join(' ');
+									}
+									break;
+							}
+
 							// Create Property based on the dialog's fields.
 							// Call client to inject it throughout.
 							var property = 
 							{
-								id: 0,
-
-
-
-
-
-
+								id: m_property.id || 0,
+								name: propertyName,
+								propertyTypeId: m_iDataType,
+								ordinal: 0,
+								initialValue: strInitialValue
 							};
 
 							if (m_strNewOrEdit === 'New') {
