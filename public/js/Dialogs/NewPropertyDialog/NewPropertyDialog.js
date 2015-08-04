80ce01a8b8e022a3518b2051c3dedf3598414ea6
Jerry Rubin <jerry@rubintech.com>
2015-08-04 11:54:01 -0400

    Uniquified property picklist values.

--- public/js/Dialogs/NewPropertyDialog/NewPropertyDialog.js
+++ public/js/Dialogs/NewPropertyDialog/NewPropertyDialog.js
@@ -113,7 +113,13 @@ define(["Core/snippetHelper", "Core/errorHelper", "Core/resourceHelper"],
 
 								$("#DataType").val("0");
 								m_iDataType = 0;
-
+								m_property = {
+									id: 0,
+                                    originalPropertyId: 0,
+                                    name: '',
+                                    initialValue: '',
+                                    ordinal: 0
+								};
 							} else {
 
 								// Edit mode for properties[m_iIndexIfEdit].
@@ -230,7 +236,8 @@ define(["Core/snippetHelper", "Core/errorHelper", "Core/resourceHelper"],
 					var m_setStateCreateBtn = function() {
 
 						var nameStatus = $("#PropertyName").val().trim().length > 0;
-						var initialValueStatus = $("#PicklistInitial").val().trim().length > 0;			// Required only for picklist (m_iDataType === 5)
+						var strPicklistInitialVal = $("#PicklistInitial").val().trim();
+						var initialValueStatus = strPicklistInitialVal.length > 0;			// Required only for picklist (m_iDataType === 5)
 
 						if (nameStatus && ((m_iDataType === 5 && initialValueStatus) || (m_iDataType !== 5 && m_iDataType > 0))) {
 
