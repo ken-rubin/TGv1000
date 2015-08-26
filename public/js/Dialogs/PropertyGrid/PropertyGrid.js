43371c0af32d5ec5daaf504d30f5c75a71d587dc
Jerry Rubin <jerry@rubintech.com>
2015-08-26 16:34:08 -0400

    All that's left is validation in Designer callback. SS

--- public/js/Dialogs/PropertyGrid/PropertyGrid.js
+++ public/js/Dialogs/PropertyGrid/PropertyGrid.js
@@ -111,7 +111,7 @@ define(["Core/snippetHelper", "Core/errorHelper", "Core/resourceHelper"],
 											name: prop.name,
 											propertyTypeId: prop.propertyTypeId,
 											initialValue: prop.initialValue,
-											currentValue: null
+											currentValue: ''
 										}
 									);
 								});
@@ -166,44 +166,47 @@ define(["Core/snippetHelper", "Core/errorHelper", "Core/resourceHelper"],
 								} else if (m.propertyTypeId === 4) {
 
 									strBuild += '<div class="PGPropCol2">True or false</div>';
-									strBuild += '<div class="PGPropCol3 style="display:inline-block;"><input id="t4_true-'+i+'" type="radio" name="b-'+i+'" value="1" ><span>true</span><input id="t4_false-'+i+'" type="radio" name="b-'+i+'" value="0" style="width:25px;"><span>false</span></div>';
-									// onclick NOT DONE
-									strBuild += '<div class="PGPropCol4"><button id="b4-'+i+'" type="button" data-toggle="tooltip" aria-label="Save" title="Save" class="btn btn-default" ><span class="glyphicon glyphicon-save" aria-hidden="true"></span></button></div>';
+									strBuild += '<div class="PGPropCol3 style="display:inline-block;"><input type="radio" name="b-'+i+'" value="1" style="width:25px;"><span>true</span>&nbsp;&nbsp;&nbsp;<input type="radio" name="b-'+i+'" value="0" style="width:25px;"><span>false</span></div>';
+									var strVal = "$('input[name=b-"+i+"]:checked').val()";
+									strBuild += '<div class="PGPropCol4"><button id="b4-'+i+'" type="button" onclick="m_functionManipulateCallback(4, &apos;' + m.name + '&apos;,&apos;' + strVal + '&apos;,null);" data-toggle="tooltip" aria-label="Save" title="Save" class="btn btn-default" ><span class="glyphicon glyphicon-save" aria-hidden="true"></span></button></div>';
 
 								} else if (m.propertyTypeId === 5) {
 
-									// Needs to change to <select>
+									// m.initialValue is used to build the select list.
+									// m.currentValue, if <> '', should be used to select current; defaulting to 'Select pick...'.
 									strBuild += '<div class="PGPropCol2">Picklist</div>';
-									strBuild += '<div class="PGPropCol3"><select id="t5-'+i+'" class="form-control"><option value="0" selected>Select pick...</option>';
+									strBuild += '<div class="PGPropCol3"><select id="t5-'+i+'" class="form-control"><option value="Select pick...">Select pick...</option>';
 
 									var pickArray = m.initialValue.match(/([\w\-]+)/g);
 									for (var j = 0; j < pickArray.length; j++) {
 
 										var pickIth = pickArray[j];
-										strBuild += '<option value="'+(j+1)+'">'+pickIth+'</option>';
+										strBuild += '<option value="'+pickIth+'">'+pickIth+'</option>';
 									}
 
 									strBuild += '</select></div>';	// close <div class="PGPropCol3" and its child <select>
-									// onclick NOT DONE
-									strBuild += '<div class="PGPropCol4"><button id="b5-'+i+'" type="button" data-toggle="tooltip" aria-label="Save" title="Save" class="btn btn-default" ><span class="glyphicon glyphicon-save" aria-hidden="true"></span></button></div>';
+									var strVal = "$('#t5-"+i+" option:selected').text()";
+									strBuild += '<div class="PGPropCol4"><button id="b5-'+i+'" type="button" onclick="m_functionManipulateCallback(5, &apos;' + m.name + '&apos;,' + strVal + ',null);" data-toggle="tooltip" aria-label="Save" title="Save" class="btn btn-default" ><span class="glyphicon glyphicon-save" aria-hidden="true"></span></button></div>';
 
 								} else if (m.propertyTypeId === 6) {
 
+									// Actual Types in project (excluding App type and current type) are used to build the select list.
+									// m.currentValue, if <> '', should be used to select current; defaulting to 'Select type...'.
 									strBuild += '<div class="PGPropCol2">Type</div>';
-									strBuild += '<div class="PGPropCol3"><select id="t6-'+i+'" class="form-control"><option value="0" selected>Select type...</option>';
+									strBuild += '<div class="PGPropCol3"><select id="t6-'+i+'" class="form-control"><option value="Select type...">Select type...</option>';
 
 									for (var j = 0; j < types.getLength(); j++) {
 
 										var clTypeIth = types.getTypeByIndex(j);
 										if (!clTypeIth.data.isApp && !(clTypeIth.data.name === m_toolInstance.type)) {
 
-											strBuild += '<option value="'+(j+1)+'">'+clTypeIth.data.name+'</option>';
+											strBuild += '<option value="'+clTypeIth.data.name+'">'+clTypeIth.data.name+'</option>';
 										}
 									}
 
 									strBuild += '</select></div>';	// close <div class="PGPropCol3" and its child <select>
-									// onclick NOT DONE
-									strBuild += '<div class="PGPropCol4"><button id="b6-'+i+'" type="button" data-toggle="tooltip" aria-label="Save" title="Save" class="btn btn-default" ><span class="glyphicon glyphicon-save" aria-hidden="true"></span></button></div>';
+									var strVal = "$('#t6-"+i+" option:selected').text()";
+									strBuild += '<div class="PGPropCol4"><button id="b6-'+i+'" type="button" onclick="m_functionManipulateCallback(6, &apos;' + m.name + '&apos;,' + strVal + ',null);" data-toggle="tooltip" aria-label="Save" title="Save" class="btn btn-default" ><span class="glyphicon glyphicon-save" aria-hidden="true"></span></button></div>';
 
 								} else {
 
@@ -224,6 +227,34 @@ define(["Core/snippetHelper", "Core/errorHelper", "Core/resourceHelper"],
 							jqInput.css("font-family", "Verdana");
 							jqInput.css("background-color", "aliceblue");
 
+							// Special processing for propertyTypeIds 4,5,6
+							for (var i = 0; i < m_properties.length; i++) {
+
+								var m = m_properties[i];
+								if (m.propertyTypeId === 4) {
+
+									if (m.currentValue !== '') {
+										// m.currentValue = '0' or '1'.
+										$('input[name=b-'+i+'][value='+m.currentValue+']').prop('checked', true);
+									}
+
+								} else if (m.propertyTypeId === 5) {
+
+									var jq5 = $("#t5-"+i);
+									if (m.currentValue === '')
+										jq5.val('Select pick...');
+									else
+										jq5.val(m.currentValue);
+
+								} else if (m.propertyTypeId === 6) {
+
+									var jq6 = $("#t6-"+i);
+									if (m.currentValue === '')
+										jq6.val('Select type...');
+									else
+										jq6.val(m.currentValue);
+								}
+							}
 						} catch (e) {
 
 							errorHelper.show(e);
