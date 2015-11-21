f98b894f4b38d3e921ecffb0a51108467fbf6e74
Jerry Rubin <jerry@rubintech.com>
2015-11-21 09:37:20 -0500

    Now need to style the list boxes and fill the data.

--- public/js/login.js
+++ public/js/login.js
@@ -4,9 +4,6 @@
 // Return null--no module object.
 //
 
-// Define some app-globals.
-var clientLogin = null;
-
 // Invoke callback when DOM is fully loaded.
 $(document).ready(function () {
 	
@@ -19,11 +16,11 @@ $(document).ready(function () {
 				try {
 
 					// Allocate and initialize the client.
-					clientLogin = new ClientLogin();
-					var exceptionRet = clientLogin.create(/*iUserId -- eventually from server specified cookie*/);
+					m_clientLogin = new ClientLogin();
+					var exceptionRet = m_clientLogin.create(/*iUserId -- eventually from server specified cookie*/);
 					if (exceptionRet) { throw exceptionRet; }
 
-					exceptionRet = m_functionLoadThreeLists();
+					exceptionRet = m_functionLoadThreeLists(errorHelper);
 					if (exceptionRet) { throw exceptionRet; }
 
 	                // Wire up the enroll button
@@ -34,7 +31,7 @@ $(document).ready(function () {
 	                });
 
 	                // Get the signed in user id from a cookie.
-	                var strUserName = clientLogin.getTGCookie("userName");
+	                var strUserName = m_clientLogin.getTGCookie("userName");
 	                if (strUserName && strUserName.length > 0) {
 
 	                    $("#inputName").val(strUserName);
@@ -126,53 +123,12 @@ $(document).ready(function () {
 	}
 });
 
-var m_functionLoadThreeLists = function() {
-
-	try {
-
-		var posting = $.post("/BOL/ProjectBO/RetrieveProjectsForLists", 
-			{},
-			'json');
-		posting.done(function(data){
-
-			if (data.success) {
-
-				$("#FreeList").empty();
-				$("#ProductList").empty();
-				$("#ClassList").empty();
-
-				data.frees.items.forEach(function(free){
-					var i = 0;
-				});
-
-				data.products.items.forEach(function(product){
-					var j=0;
-				});
-
-				data.classes.items.forEach(function(klass){
-					var k= 0;
-				});
-
-				return null;
-
-			} else {
-
-				// !data.success
-				return new Error(data.message);
-			}
-		});
-	} catch (e) {
-
-		return e;
-	}
-}
-
 var m_functionEnrollButtonClick = function(errorHelper) {
 	
 	try {
 
 		// Ask client to show the enroll dialog.
-		var exceptionRet = clientLogin.showEnrollDialog(m_functionOnGotNewEnrollee);
+		var exceptionRet = m_clientLogin.showEnrollDialog(m_functionOnGotNewEnrollee);
 		if (exceptionRet) {
 
 			throw exceptionRet;
@@ -237,3 +193,141 @@ var m_functionSignInButtonClick = function(errorHelper) {
     }
 }
 
+var m_functionLoadThreeLists = function(errorHelper) {
+
+	try {
+
+		var posting = $.post("/BOL/ProjectBO/RetrieveProjectsForLists", 
+			{},
+			'json');
+		posting.done(function(data){
+
+			if (data.success) {
+
+				m_frees = data.frees.items;
+				m_products = data.products.items;
+				m_classes = data.classes.items;
+
+				$.ajax({
+
+					cache: false,
+					data: { 
+
+						templateFile: "Login/Listboxes/freeBox"
+					}, 
+					dataType: "HTML",
+					method: "POST",
+					url: "/renderJadeSnippet"
+				}).done(m_functionRenderJadeSnippetResponseFree).error(errorHelper.show);
+
+				$.ajax({
+
+					cache: false,
+					data: { 
+
+						templateFile: "Login/Listboxes/productBox"
+					}, 
+					dataType: "HTML",
+					method: "POST",
+					url: "/renderJadeSnippet"
+				}).done(m_functionRenderJadeSnippetResponseProduct).error(errorHelper.show);
+
+				$.ajax({
+
+					cache: false,
+					data: { 
+
+						templateFile: "Login/Listboxes/classBox"
+					}, 
+					dataType: "HTML",
+					method: "POST",
+					url: "/renderJadeSnippet"
+				}).done(m_functionRenderJadeSnippetResponseClass).error(errorHelper.show);
+
+				return null;
+
+			} else {
+
+				// !data.success
+				return new Error(data.message);
+			}
+		});
+	} catch (e) {
+
+		return e;
+	}
+}
+
+var m_functionRenderJadeSnippetResponseFree = function(htmlData) {
+
+	m_freeHTML = htmlData;
+	if (m_productHTML && m_classHTML) {
+
+		m_functionFillTheLists();
+	}
+}
+
+var m_functionRenderJadeSnippetResponseProduct = function(htmlData) {
+
+	m_productHTML = htmlData;
+	if (m_freeHTML && m_classHTML) {
+
+		m_functionFillTheLists();
+	}
+}
+
+var m_functionRenderJadeSnippetResponseClass = function(htmlData) {
+
+	m_classHTML = htmlData;
+	if (m_productHTML && m_freeHTML) {
+
+		m_functionFillTheLists();
+	}
+}
+
+var m_functionFillTheLists = function() {
+
+
+	$("#FreeList").empty();
+	for(var i = 0; i < m_frees.length; i++) {
+
+		var freeIth = m_frees[i];
+		$("#FreeList").append('<div id="Free_' + i + '">' + m_freeHTML + '</div>');
+
+		// Fill the data in Free_i.
+
+
+	}
+
+	$("#ProductList").empty();
+	for(var i = 0; i < m_products.length; i++) {
+
+		var productIth = m_products[i];
+		$("#ProductList").append('<div id="Product_' + i + '">' + m_productHTML + '</div>');
+
+		// Fill the data in Product_i.
+
+
+	}
+
+	$("#ClassList").empty();
+	for(var i = 0; i < m_classes.length; i++) {
+
+		var classIth = m_classes[i];
+		$("#ClassList").append('<div id="Class_' + i + '">' + m_classHTML + '</div>');
+
+		// Fill the data in Class_i.
+
+
+	}
+
+}
+
+// Define some app-globals.
+var m_clientLogin = null;
+var m_frees = [];
+var m_products = [];
+var m_classes = [];
+var m_freeHTML = null;
+var m_productHTML = null;
+var m_classHTML = null;
